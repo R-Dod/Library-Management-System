a9ead947f63f1d30530c19326b94642a8d4e9469
unknown <r.dodani_18551@khi.iba.edu.pk>
2021-12-04 19:31:47 +0500

    Books detail+copies table

--- pages/books/[bookid].tsx
+++ pages/books/[bookid].tsx
@@ -1,21 +1,165 @@
 import { NextPage } from "next";
 import { useRouter } from 'next/router';
 import { GetStaticProps, GetStaticPaths } from "next";
-//import { Book } from "@material-ui/icons";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import DataTable from "../../shared-components/data-table";
+const copies: any[] = [
+    { field: 'sno', headerName: '#', type:'number', width: 10 },
+    { field: 'id', headerName: 'ID', type:'number', width: 10 },
+    { field: 'status', headerName: 'Status', width: 130 },
+    { field: 'ShelfNo', headerName: 'Shelf No.', width: 130 }
+];
 
+const rows: any = [
+    {
+        sno: 1,
+        id: 100,
+        status: 'Checked Out',
+        ShelfNo: '234'
+    },
+    {
+        sno:2,
+        id: 101,
+        status: 'Available',
+        ShelfNo: '345'
+    },
+    {
+        sno: 3,
+        id: 102,
+        status: 'Checked Out',
+        ShelfNo: '2340'
+    },
+    {
+        sno:4,
+        id: 103,
+        status: 'Available',
+        ShelfNo: '3450'
+    },
+    {
+        sno: 5,
+        id: 104,
+        status: 'Checked Out',
+        ShelfNo: '2341'
+    },
+    {
+        sno:6,
+        id: 105,
+        status: 'Available',
+        ShelfNo: '3451'
+    },        
+];
 
 const BookPage: NextPage<any, any> = function ({ book }) {
     const router = useRouter();
-
     return (
-        <div>
-            <h2>
-                {book.id} {book.title} | {book.author_id}
-            </h2>
-            <h3>{book.pages} Pages  {book.releaseDate}</h3>
-            <p>{book.isbn}</p>
-        </div>
+    <div>
+        <h1>{book.title}</h1>
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '100%' },}}
+      noValidate
+      autoComplete="off"
+    >
+        <TextField
+          id="id"
+          label="ID"
+          defaultValue={book.id}
+          InputProps={{
+            readOnly: true,
+          }}
+          variant="standard"
+        />        
+       <h1> <TextField
+          required
+          id="title"
+          label="Title"
+          defaultValue={book.title}
+          variant="standard"
+        /></h1>
+        <TextField
+          id="author"
+          label="Author"
+          defaultValue={book.Author}
+          variant="standard"
+        />
+        <TextField
+          id="category"
+          label="category"
+          defaultValue={book.Category}
+          variant="standard"
+        />
+          <TextField
+          id="publisher"
+          label="Publisher"
+          defaultValue={book.publisher}
+          variant="standard"
+        />
+          <TextField
+          id="date"
+          //type="date"
+          label="Date"
+          defaultValue={book.date}
+          variant="standard"
+        />
+
+        <TextField
+          id="price"
+          label="Cost"
+          type="number"
+          defaultValue={book.cost}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="standard"
+        />
+        <TextField
+          id="isbn"
+          label="ISBN"
+          defaultValue={book.ISBN}
+          variant="standard"
+        />        
+
+        <TextField
+          id="standard-multiline-static"
+          label="Description"
+          multiline
+          defaultValue={book.Description}
+          variant="standard"
+        />
+
+      </Box>
+        
+      <div style={{ height: 400, width: '100%' }}>
+          <DataTable
+                        options={{
+                            showSelectAllCheckbox: false,
+                            exportButton: false,
+                            draggable: false,
+                            search: false,
+                          }}
+      title={'All Copies'}
+              columns={copies}              
+              data={rows}
+
+              />
+    </div>
+    <button>Save</button>
+
+    </div>
     )
+        // <div>
+        //     <h2>
+        //         {book.id} {book.title} | {book.date}
+        //     </h2>
+        //     <h3>Rs. {book.cost}</h3>
+        //     <h4>{book.ISBN}</h4>
+        //     <p>{book.Description}</p>
+        //     <button>Save</button>
+        // </div>       
+    
 //   console.log(router.query);
 //   // Get ID from router.query and fetch data for that id
 //   // replace it by returned data from API
@@ -43,9 +187,12 @@ const BookPage: NextPage<any, any> = function ({ book }) {
 
 export default BookPage
 
+
+
+
 export const getStaticProps: GetStaticProps = async (context) =>{
     const { params } = context
-    const response = await fetch(`https://my-json-server.typicode.com/dmitrijt9/book-api-mock/books/${params.bookid}`)
+    const response = await fetch(`http://localhost:4000/books/${params.bookid}`)
     const data = await response.json()
     return {
         props: {
@@ -55,7 +202,7 @@ export const getStaticProps: GetStaticProps = async (context) =>{
 }
 
 export const getStaticPaths: GetStaticPaths = async () =>{
-    const response = await fetch(`https://my-json-server.typicode.com/dmitrijt9/book-api-mock/books/`)
+    const response = await fetch(`http://localhost:4000/books`)
     const data = await response.json()
 
     const paths = data.map((book) => {
--- pages/books/index.js
+++ pages/books/index.js
@@ -1,29 +1,59 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function BookList({ books }) {
+
+function BookList({ books }) {  
+
     return (
         <>
-        <h1>List of Books</h1>
-        {books.map( book => {
-            return (
-                <div key={book.id}>
-                    <Link href = {`books/${book.id}`}>
-                    <h2>
-                        {book.id} {book.title} {book.isbn}
-                    </h2>
-                    </Link>
-                    <hr />
-                </div>
-            )
-        })}
-    </>
+ <h1>List of Books</h1>
+        <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+                    <TableHead>
+                        <button>Add</button>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Title</TableCell>
+                        <TableCell>Author(s)</TableCell>
+                        <TableCell>Category(s)</TableCell>
+                        <TableCell>Publisher</TableCell>
+                        <TableCell>Cost</TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {books.map((book) => (
+                            <TableRow key={book.id}>
+                                <Link href = {`books/${book.id}`}><a>
+                                    <TableCell component="th" scope="row">{book.id}</TableCell>
+                                    <TableCell >{book.title}</TableCell>
+                                    <TableCell >{book.Author}</TableCell>
+                                    <TableCell >{book.Category}</TableCell>
+                                    <TableCell >{book.publisher}</TableCell>
+                                    <TableCell >{book.cost}</TableCell>
+                                    </a>
+                                    </Link>
+                                    <TableCell><Link href = "#">Delete</Link></TableCell>
+                                    </TableRow>
+                                    ))}
+                                    </TableBody>
+                                    </Table>
+                                    </TableContainer>
+    </>    
     )
 }
+
+
 
 export default BookList
 
 export async function getStaticProps() {
-    const response = await fetch('https://my-json-server.typicode.com/dmitrijt9/book-api-mock/books')
+    const response = await fetch('http://localhost:4000/books')
     const data = await response.json()
 
     return {
