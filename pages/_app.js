d446196da13c67f1c86baa63b9e3fd15391ad84c
NoorulainEhtisham <noorulain.ehtisham@gmail.com>
2021-12-06 19:51:22 +0500

    grid added _app, issuehistory by member

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
 import '../styles/global.css'
 import Navbar from '../components/Navbar/Navbar';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
 export default function App({ Component, pageProps }) {
-    return (
+  return (
     <>
-      <Navbar/> 
+
+      <Navbar />
       <div>Welcome User!</div>
-      <Component {...pageProps} />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid item xs={1} />
+          <Grid item xs={10}>
+            <Component {...pageProps} />
+          </Grid>
+          <Grid item xs={1} />
+        </Grid>
+
+      </Box>
     </>
-    );
-    
-  }
+  );
+
+}
